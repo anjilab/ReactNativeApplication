c5c4141abfffcd10d4d8277f83ffe19fb3bdf898
alijna <anzilabudathoki@gmail.com>
2019-07-31 17:32:12 +0545

    [Day1Application] Login design

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { AppRegistry, StyleSheet, Text, View } from "react-native";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button
+} from "react-native";
 
 // export default function App() {
 //   return (
@@ -9,28 +17,77 @@ import { AppRegistry, StyleSheet, Text, View } from "react-native";
 //   );
 // }
 class Greetings extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  };
+  welcomeFamily= ()=>{
+    console.warn('Welcome to Young Innovations Family');
+  }
+
   render() {
     return (
       <View style={styles.outerContainer}>
-        <Text style={{ color: "#000" }}>Hello {this.props.name} !!! </Text>
+        <Image
+          source={require("./assets/yipl1.png")}
+          style={styles.imageStyle}
+        />
+
+        {/* <Text style={styles.contentStyle}>Hello {this.props.name} !!! </Text> */}
+        <Text style={styles.headingContent}>Login</Text>
+
+        <View>
+          <Text style={{ paddingBottom: 10, paddingTop: 10, fontSize: 16 }}>
+            Email
+          </Text>
+          <TextInput
+            style={styles.inputContent}
+            placeholder="Email....."
+            onChangeText={email =>
+              console.log(email) || this.setState({ email: email })
+            }
+          />
+          <Text style={{ paddingBottom: 10, paddingTop: 10, fontSize: 16 }}>
+            Password
+          </Text>
+          <TextInput
+            style={styles.inputContent}
+            placeholder="Password....."
+            onChangeText={password => this.setState({ password: password })}
+          />
+        </View>
+
+        {/* <View style={{paddingTop:10, flex:1,width:'80%', alignItems:'flex-start'}}>
+          <Button title="Login" color="red" onPress={this.welcomeFamily} />
+        </View> */}
+
+         <View style={styles.footerContent}>
+           <Text>Already a member of YIPL family?</Text>
+          <Button title="Login" onPress={this.welcomeFamily} />
+          <Text>Register to join the family.</Text>
+          <Button title="Register" onPress={this.welcomeFamily} />
+
+        </View> 
+
+
       </View>
     );
   }
 }
-class DesignGreeting extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>This is design view</Text>
-      </View>
-    );
-  }
-}
+// class DesignGreeting extends React.Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text>This is design view</Text>
+//       </View>
+//     );
+//   }
+// }
 
 export default class LotsOfGreetings extends React.Component {
   render() {
     return (
-      <View style={styles.greetingContainer}>
+      <View style={styles.mainContainer}>
         <Greetings name="Anjila" />
       </View>
     );
@@ -43,17 +100,46 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center"
   },
+  mainContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
   outerContainer: {
     height: "60%",
     width: 300,
-    backgroundColor: "#c8ccc9",
-    borderRadius: 8
+    backgroundColor: "#d2d2d2",
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 10,
+    elevation: 30,
+    alignItems: "center"
   },
-  greetingContainer: {
-    flex: 1,
-    backgroundColor: "#000",
-    alignItems: "center",
-    justifyContent: "center"
+  headingContent: {
+    color: "#000",
+    fontSize: 30
+  },
+  imageStyle: {
+    resizeMode: "contain",
+    height: 90,
+    width: 150
+  },
+  inputContent: {
+    height: 40,
+    width: 250,
+    backgroundColor: "#fff",
+    padding:10,
+    borderRadius:8
+  },
+  footerContent:{
+    margin:30
+
   }
 });
 
